bd535385512baab054e733c7c9d6202c84098c14
LIJA30 <jade.li@oocl.com>
2025-09-10 10:56:01 +0800

    refactor: update to integrating test should_return_employee_when_employee_found

--- src/test/java/com/example/demo/EmployeeControllerTest.java
+++ src/test/java/com/example/demo/EmployeeControllerTest.java
@@ -76,19 +76,19 @@ public class EmployeeControllerTest {
                 .andExpect(jsonPath("$.length()").value(2));
     }
 
-//    @Test
-//    void should_return_employee_when_employee_found() throws Exception {
-//        Employee expect = employeeController.createEmployee(johnSmith());
-//
-//        mockMvc.perform(get("/employees/1")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(expect.getId()))
-//                .andExpect(jsonPath("$.name").value(expect.getName()))
-//                .andExpect(jsonPath("$.age").value(expect.getAge()))
-//                .andExpect(jsonPath("$.gender").value(expect.getGender()))
-//                .andExpect(jsonPath("$.salary").value(expect.getSalary()));
-//    }
+    @Test
+    void should_return_employee_when_employee_found() throws Exception {
+        Employee expect = createJohnSmith();
+
+        mockMvc.perform(get("/employees/" + expect.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(expect.getId()))
+                .andExpect(jsonPath("$.name").value(expect.getName()))
+                .andExpect(jsonPath("$.age").value(expect.getAge()))
+                .andExpect(jsonPath("$.gender").value(expect.getGender()))
+                .andExpect(jsonPath("$.salary").value(expect.getSalary()));
+    }
 //
 //    @Test
 //    void should_return_male_employee_when_employee_found() throws Exception {
